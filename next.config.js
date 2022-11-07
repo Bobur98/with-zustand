1ef5b8132e48fc61c23c1d1c282d6c7c25e6d1f8
Boburmirzo Muhibulalev <bmuhibullaev@maiul.ru>
2022-11-07 18:51:44 +0900

    commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    domains: [`http://localhost:3000/`],
+  },
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
